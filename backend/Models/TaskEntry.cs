ea1b0cf239a2afb9aa7cea4322a6d66a5c409e44
jhands0 <jamie_hands@yahoo.co.uk>
2025-02-23 20:27:48 +0000

    Created a Task Entry data model

---
+++ backend/Models/TaskEntry.cs
@@ -0,0 +1,15 @@
+namespace backend.Models;
+
+public class TaskEntry
+{
+    public long Id { get; set; }
+    public int RoomNo { get; set; }
+    public string Task { get; set; }
+    public int Code { get; set; }
+    public string Comments { get; set; }
+    public bool Completed { get; set; }
+    public DateTime Date { get; set; }
+    public int Priority { get; set; }
+
+    # TODO: Change RoomNo, Code and Priority records to their own Enums
+}
