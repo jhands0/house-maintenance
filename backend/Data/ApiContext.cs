4d85810f42c0234d260c77a60e39785400b730f3
jhands0 <jamie_hands@yahoo.co.uk>
2025-02-24 20:51:49 +0000

    Added the framework for a In-Memory Database

---
+++ backend/Data/ApiContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace backend.Data;
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<TaskEntry> Tasks { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
--- backend/Models/TaskEntry.cs
+++ backend/Models/TaskEntry.cs
@@ -1,15 +1,16 @@
 namespace backend.Models;
-
-public class TaskEntry
 {
-    public long Id { get; set; }
-    public int RoomNo { get; set; }
-    public string Task { get; set; }
-    public int Code { get; set; }
-    public string Comments { get; set; }
-    public bool Completed { get; set; }
-    public DateTime Date { get; set; }
-    public int Priority { get; set; }
+    public class TaskEntry
+    {
+        public long Id { get; set; }
+        public int RoomNo { get; set; }
+        public string Task { get; set; }
+        public int Code { get; set; }
+        public string Comments { get; set; }
+        public bool Completed { get; set; }
+        public DateTime Date { get; set; }
+        public int Priority { get; set; }
 
-    # TODO: Change RoomNo, Code and Priority records to their own Enums
+        # TODO: Change RoomNo, Code and Priority records to their own Enums
+    }
 }
