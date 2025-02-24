44834be96b7145b2a6d6920055684830f312b3bf
jhands0 <jamie_hands@yahoo.co.uk>
2025-02-24 20:58:24 +0000

    Registered the Taskdb DbContext to the builder to make an In Memory Database when the program is ran

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase("TaskDb"));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
