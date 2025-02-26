d9e1ec227adfb34131ac3ba6e7c597e430132d17
jhands0 <jamie_hands@yahoo.co.uk>
2025-02-26 11:54:17 +0000

    Added Column type annotations for the task object for when I migrate over to SQL server

--- backend/Models/TaskEntry.cs
+++ backend/Models/TaskEntry.cs
@@ -1,14 +1,29 @@
-namespace backend.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models
 {
     public class TaskEntry
     {
+        [Key]
         public long Id { get; set; }
+        
         public int RoomNo { get; set; }
+        
+        [Column(TypeName = "nvarchar(100)")]
         public string Task { get; set; }
+        
         public int Code { get; set; }
+        
+        [Column(TypeName = "tinytext")]
         public string Comments { get; set; }
+
+        [Column(TypeName = "bool")]
         public bool Completed { get; set; }
+
+        [Column(TypeName = "date")]
         public DateTime Date { get; set; }
+
         public int Priority { get; set; }
 
         # TODO: Change RoomNo, Code and Priority records to their own Enums
