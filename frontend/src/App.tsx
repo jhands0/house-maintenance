c7b3af595b67be14446b2163bb867f5bdbf83468
jhands0 <jamie_hands@yahoo.co.uk>
2025-02-28 13:08:34 +0000

    Integrated FluentUI into the main page of the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,13 @@ import './App.css';
 import axios, { AxiosResponse } from 'axios';
 import { getAllUrl } from './endpoint';
 
+import { provideFluentDesignSystem, fluentCard, fluentButton } from '@fluentui/web-components';
+import { provideReactWrapper } from '@microsoft/fast-react-wrapper';
+
+const { wrap } = provideReactWrapper(React, provideFluentDesignSystem());
+export const FluentCard = wrap(fluentCard());
+export const FluentButton = wrap(fluentButton());
+
 function App() {
 
   useEffect(() => {
@@ -13,12 +20,15 @@ function App() {
   }, [])
 
   return (
-    <>
+    <FluentCard>
         <h1>
             House Maintenance Website
         </h1>
         <p> Communicating with an ASP .NET Core server </p>
-    </>
+        <FluentButton appearance="accent" onClick={() => console.log('clicked')}>
+            Click Me!
+        </FluentButton>
+    </FluentCard>
   );
 }
 
